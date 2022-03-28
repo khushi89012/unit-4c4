9eebb9d381f3e95210f40041ecdec7fed658d437
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-03-28 11:22:03 +0530

    ok

--- user.model.js
+++ user.model.js
@@ -13,4 +13,22 @@ const userSchema = new mongoose.Schema({
 
 })
 
+userSchema.pre("save" , function(next){
+    let hash =  bcrypt.hashSync(this.password, 8)
+    this.password = hash
+
+    return next()
+})
+//  we have to decrypt the password
+
+userSchema.methods.checkpassword = function(password){
+
+ return  bcrypt.compareSync(password, this.password)
+
+
+}
+
+
+
+
 mongoose.exports = mongoose.model("todouser",userSchema)
